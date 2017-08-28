90638589490c8eb60bdc7bef5346b7bb53c49ab9
carrordeng <carror1994@gmail.com>
2017-08-28 23:35:39 +0800

    One project

---
+++ Chapter 1/src/Sort.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String[] input = new String[N];
+        for (int i = 0; i < N; i++) {
+            input[i] = sc.next();
+        }
+        Arrays.sort(input);
+        for(String str : input) {
+            System.out.println(str);
+        }
+    }
+}
